70a1ae64c449c26e047814cb423db9ebe939b977
Bishwajit <bbaruaca@gmail.com>
2019-10-02 23:43:55 -0400

    update

--- src/components/Utilities/projectListData.js
+++ src/components/Utilities/projectListData.js
@@ -7,97 +7,107 @@ let access='http://howtolearn.me/wp-content/uploads/2014/04/Microsoft-Access-Log
 let selenium='https://qph.fs.quoracdn.net/main-qimg-30b78130863ea42a08b57d14a591f97d';
 
 export const projectListData=[
-{
-	projectId:1,
-	projectType:'React',
-	projectTite:'My potfolio based website',
-	imagelink:react,
-	summary:'WAIT 30 SECOND FOR FIRST TIME LOAD. Hosted in Herokuapp.com. This website based on my potfolio and business exposure. Tools uses: React, react-mdl, react-router-dom, bootstrap, tachyons, font awesome, google font',
-	github:'https://github.com/bishwajitbd/bworldca',
-	liveLink:'https://bworldca.herokuapp.com/',
-	projectdemo:'https://www.youtube.com/watch?v=2cQHvZliN7o'
-},
-{
-	projectId:2,
-	projectType:'ASP.NET',
-	projectTite:'Membership servics app',
-	imagelink:asp,
-	summary:'WAIT 30 SECOND FOR FIRST TIME LOAD. Hosted at https://www.smarterasp.net/. Entertainment based products sales here.',
-	github:'https://github.com/bishwajitbd/bworldca',
-	liveLink:'http://bbaruaca-001-site1.etempurl.com/',
-	projectdemo:'https://www.youtube.com/watch?v=Y-sK3CG08q0'
-},
+	{
+		projectId:1,
+		projectType:'React',
+		projectTite:'My potfolio based website',
+		imagelink:react,
+		summary:'WAIT 30 SECOND FOR FIRST TIME LOAD. Hosted in Herokuapp.com. This website based on my potfolio and business exposure. Tools uses: React, react-mdl, react-router-dom, bootstrap, tachyons, font awesome, google font',
+		github:'https://github.com/bishwajitbd/bworldca',
+		liveLink:'https://bworldca.herokuapp.com/',
+		projectdemo:'https://www.youtube.com/watch?v=2cQHvZliN7o'
+	},
+	{
+		projectId:2,
+		projectType:'ASP.NET',
+		projectTite:'Membership servics app',
+		imagelink:asp,
+		summary:'WAIT 30 SECOND FOR FIRST TIME LOAD. Hosted at https://www.smarterasp.net/. Entertainment based products sales here.',
+		github:'https://github.com/bishwajitbd/bworldca',
+		liveLink:'http://bbaruaca-001-site1.etempurl.com/',
+		projectdemo:'https://www.youtube.com/watch?v=Y-sK3CG08q0'
+	},
 
-{
-	projectId:2,
-	projectType:'ASP.NET',
-	projectTite:'SOAP API',
-	imagelink:asp,
-	summary:'Demonestrate SOAP API using WCF. There are three apps, one is client and two services realated. Backend data comes from SQL server uisng store porcedure.',
-	github:'https://github.com/bishwajitbd/WCF_REST_API_DistributedApp_Music',
-	liveLink:'#',
-	projectdemo:'https://www.youtube.com/watch?v=HRKEfxubMEk'
-},
-{
-	projectId:3,
-	projectType:'Android',
-	projectTite:'Tic-Tac-Toe game',
-	imagelink:android,
-	summary:'Tic-Toc-Toe App is a dynamic app that store data in the sqlite.',
-	github:'https://github.com/bishwajitbd/Android_Tic_Toc_Toe',
-	liveLink:'#',
-	projectdemo:'#'
-},
-{
-	projectId:3,
-	projectType:'Android',
-	projectTite:'Bloging app',
-	imagelink:android,
-	summary:'Easy to post blog and share it within your team. Data store in your Firebase database',
-	github:'https://github.com/bishwajitbd/BloggingProject_Android',
-	liveLink:'#',
-	projectdemo:'#'
-},
-{
-	projectId:4,
-	projectType:'Python',
-	projectTite:'REST API',
-	imagelink:python,
-	summary:'Demonestrate REST API using Python and flash.',
-	github:'https://github.com/bishwajitbd/Exercise3JPAApplication',
-	liveLink:'#',
-	projectdemo:'#'
-},
-{
-	projectId:5,
-	projectType:'Java',
-	projectTite:'JPA exersise',
-	imagelink:java,
-	summary:'Demonestrate JPA exersise using Java.',
-	github:'https://github.com/bishwajitbd/Exercise3JPAApplication',
-	liveLink:'#',
-	projectdemo:'#'
-},
-{
-	projectId:6,
-	projectType:'Access',
-	projectTite:'Motion Concept',
-	imagelink:access,
-	summary:'Client App. Developed complain tracking and sticker printing',
-	github:'https://github.com/bishwajitbd/DistubutedComputerAssi3',
-	liveLink:'#',
-	projectdemo:'#'
-},
-{
-	projectId:7,
-	projectType:'Selenium',
-	projectTite:'A complete Testing Framework (Java, Selenium, TestNG)',
-	imagelink:selenium,
-	summary:'A complete testing framework following page object method using Java, Selenium, TestNG, Log4j2, screenshot, and more.',
-	github:'https://github.com/bishwajitbd/TestingFrameworkWithSelenum',
-	liveLink:'https://www.youtube.com/watch?v=Vvm8fw9K_S4',
-	projectdemo:'https://www.youtube.com/watch?v=Vvm8fw9K_S4'
-}
+	{
+		projectId:2,
+		projectType:'ASP.NET',
+		projectTite:'SOAP API',
+		imagelink:asp,
+		summary:'Demonestrate SOAP API using WCF. There are three apps, one is client and two services realated. Backend data comes from SQL server uisng store porcedure.',
+		github:'https://github.com/bishwajitbd/WCF_REST_API_DistributedApp_Music',
+		liveLink:'#',
+		projectdemo:'https://www.youtube.com/watch?v=HRKEfxubMEk'
+	},
+	{
+		projectId:3,
+		projectType:'Android',
+		projectTite:'Tic-Tac-Toe game',
+		imagelink:android,
+		summary:'Tic-Toc-Toe App is a dynamic app that store data in the sqlite.',
+		github:'https://github.com/bishwajitbd/Android_Tic_Toc_Toe',
+		liveLink:'#',
+		projectdemo:'#'
+	},
+	{
+		projectId:3,
+		projectType:'Android',
+		projectTite:'Bloging app',
+		imagelink:android,
+		summary:'Easy to post blog and share it within your team. Data store in your Firebase database',
+		github:'https://github.com/bishwajitbd/BloggingProject_Android',
+		liveLink:'#',
+		projectdemo:'#'
+	},
+	{
+		projectId:4,
+		projectType:'Python',
+		projectTite:'REST API',
+		imagelink:python,
+		summary:'Demonestrate REST API using Python and flash.',
+		github:'https://github.com/bishwajitbd/Exercise3JPAApplication',
+		liveLink:'#',
+		projectdemo:'#'
+	},
+	{
+		projectId:5,
+		projectType:'Java',
+		projectTite:'JPA exersise',
+		imagelink:java,
+		summary:'Demonestrate JPA exersise using Java.',
+		github:'https://github.com/bishwajitbd/Exercise3JPAApplication',
+		liveLink:'#',
+		projectdemo:'#'
+	},
+	{
+		projectId:6,
+		projectType:'Access',
+		projectTite:'Motion Concept',
+		imagelink:access,
+		summary:'Client App. Developed complain tracking and sticker printing',
+		github:'https://github.com/bishwajitbd/DistubutedComputerAssi3',
+		liveLink:'#',
+		projectdemo:'#'
+	},
+	{
+		projectId:7,
+		projectType:'Selenium',
+		projectTite:'A complete Testing Framework (Java, Selenium, TestNG)',
+		imagelink:selenium,
+		summary:'A complete testing framework following page object method using Java, Selenium, TestNG, Log4j2, screenshot, and more.',
+		github:'https://github.com/bishwajitbd/TestingFrameworkWithSelenum',
+		liveLink:'https://www.youtube.com/watch?v=Vvm8fw9K_S4',
+		projectdemo:'https://www.youtube.com/watch?v=Vvm8fw9K_S4'
+	},
+	{
+		projectId:1,
+		projectType:'React',
+		projectTite:'Shopping app - Under construction with CI/CD',
+		imagelink:react,
+		summary:'WAIT 30 SECOND FOR FIRST TIME LOAD. Hosted in Herokuapp.com.',
+		github:'https://github.com/bishwajitbd/reactEcommerce',
+		liveLink:'https://reactshoppingapp.herokuapp.com/',
+		projectdemo:'#'
+	}
 ]
 
 
---
+++ src/components/customerprofile.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class Customerprofile extends Component{
+	render(){
+		return(
+			<div><h1>Customerprofile: Under construction...</h1></div>
+		)
+	}
+}
+
+export default Customerprofile;
